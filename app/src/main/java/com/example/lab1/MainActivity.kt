79b9eebf184df8484c65bfb08ee292d297277360
goshan <goshan@iu.edu>
2025-01-21 18:27:32 -0500

    up to slide 26 completed

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -34,19 +34,11 @@ class MainActivity : AppCompatActivity1() {
 
         binding.trueButton.setOnClickListener {
                 view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+            checkAnswer(true)
         }
         binding.falseButton.setOnClickListener {
                 view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+            checkAnswer(false)
         }
         binding.nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
@@ -61,4 +53,15 @@ class MainActivity : AppCompatActivity1() {
         binding.questionTextView.setText(questionTextResId)
     }
 
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
 }
